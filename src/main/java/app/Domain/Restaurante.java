8685de46678516f2d6d4685b74d35ad4bda24559
SEBAS <sebascesde16@gmail.com>
2026-08-22 15:46:52 -0500

    **Commit de sebas creacion clase Restaurante**

---
+++ src/main/java/app/Domain/Restaurante.java
@@ -0,0 +1,72 @@
+package app.Domain;
+
+public class Restaurante {
+
+    //Atributos
+
+    private Integer id;
+    private String nombre;
+    private String ubicacion;
+    private String horario;
+
+
+    //Setters Getters
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        ubicacion = ubicacion;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
+
+    //Metodos
+
+    public void create(){
+
+    }
+
+    public void selectAll(){
+
+    }
+
+    public void selectById(int id){
+
+    }
+
+    public void update(){
+
+    }
+
+    public void deleteById(){
+
+    }
+
+
+}
+
