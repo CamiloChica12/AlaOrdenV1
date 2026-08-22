446ae6921827a373b4bf22e5ad2b2822ae7bfc22
lalvarezg@cesde.net <miguelluisalvarezgomez@gmail.com>
2026-08-22 16:27:04 -0500

    ***creacion clase pedido por luis***

---
+++ src/main/java/app/Domain/Pedido.java
@@ -0,0 +1,41 @@
+package app.Domain;
+
+public class Pedido {
+    private int id ;
+    private String estado;
+    private String fecha;
+
+
+    //Constructor con parametros
+    public Pedido (int id, String estado, String fecha){
+        this.id = id;
+        this.estado = estado;
+        this.fecha = fecha;
+    }
+
+
+
+
+    //Methodost
+
+    public void realizarPedido(){
+
+    }
+
+    public void cambiarEstado(){
+
+    }
+
+    public void calcularTotal(){
+
+    }
+
+    public void generarQr(){
+
+    }
+
+
+
+
+
+}
